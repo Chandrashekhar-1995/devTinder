2559cb04d3d88e11dde9444ac10148da153d15cd
Chandra Shekhar <jmbd.group@gmail.com>
2024-11-23 13:25:37 +0530

    request routing part 1

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -3,12 +3,18 @@ const requestRouter = express.Router();
 const userAuth = require("../middleware/auth");
 
 
-requestRouter.post("/request/send/:status/:ToUserId", userAuth, async(req, res) => {
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async(req, res) => {
     
     try {
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+
         const user = req.user;
-        res.send( user.firstName + " sending a connection request");
-        res.send(" sending a connection request");
+        res.json({
+            message: `${fromUserId} is ${status} so he sent request to ${toUserId}`,
+        })
     } catch (err) {
         res.status(400).send("Error : " + err.message);
     }
